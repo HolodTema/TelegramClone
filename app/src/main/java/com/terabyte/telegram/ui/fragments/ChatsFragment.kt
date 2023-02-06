8c31502642986240911e0999262e7cd4e9a63852
HolodTema <holod.tema13062006@gmail.com>
2023-02-06 21:27:37 +0800

    Telegram app after 5th video from Youtube. Little code refactoring happened.

---
+++ app/src/main/java/com/terabyte/telegram/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,11 @@
+package com.terabyte.telegram.ui.fragments
+
+import com.terabyte.telegram.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
---
+++ app/src/main/java/com/terabyte/telegram/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.terabyte.telegram.ui.fragments
+
+import com.terabyte.telegram.R
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
